81b59b06a82cae162bb687cb626b62d211145cae
juantinobr.as <juantinobr.as@gmail.com>
2024-06-27 04:31:32 +0000

    add firebase setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:manajemen_waktu/firebase_options.dart';
 
 import 'screens/welcome_screens.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
